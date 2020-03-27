c54eb0c5c77f3fcd8c15a959c57e5bc21e9b8840
Ricardo Costa <re92252z@RicardoMJC.nimbus.local>
2020-03-27 08:53:39 -0300

    Deletei o back

--- backend/src/utils/parseStringAsArray.js
+++
@@ -1,3 +0,0 @@
-module.exports = function parseStringAsArray(arrayAsString) {
-    return arrayAsString.split(',').map(tech => tech.trim()); //split(separa todos os arrays), map(percorre o array), trim(remove espaçamento antes e depois de uma string)
-}
